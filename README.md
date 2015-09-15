f68c65cba65406ad2554deef65f96681be23a08a
Dario Diaz <darioo.diaz1@gmail.com>
2015-09-15 09:12:56 -0300

    cleaning

--- README.md
+++
@@ -1,2 +0,0 @@
-# proyecto-moviles-utn
-Proyecto final de la materia TDAM de UTN FRC
--- gen/com/utn/tdam_tp_final/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.utn.tdam_tp_final;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
